d7ce05362731172df5bb6c6d28ad9fdb6ad888ed
Mariamchai <mari.chaidze@gmail.com>
2024-07-16 03:23:54 -0700

    commit

--- day05/homework/04.py
+++ day05/homework/04.py
@@ -1,12 +1,12 @@
 #how to add numbers
-num1 = input(int("what's the first number you wanna add?"))
-num2 = input(int("what's the second number you wanna add?"))
+num1 = int(input("what's the first number you wanna add?"))
+num2 = int(input("what's the second number you wanna add?"))
 print("your total answer is: ", num1 + num2)
 #how to subtract
-numb1 = input(int("first number?"))
-numb2 = input(int("second number?"))
-print(numb1 + numb2)
+numb1 = int(input("first number?"))
+numb2 = int(input("second number?"))
+print(numb1 - numb2)
 #how to multiply and divide
-number = input(int("first one?"))
-number1 = input(int("second one?"))
+number = int(input("first one?"))
+number1 = int(input("second one?"))
 print(number * number1, number // number1)

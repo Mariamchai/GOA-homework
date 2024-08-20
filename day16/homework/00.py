348bf7fdcefc5c3a1ec0062996b044b75f827c4c
Mariamchai <mari.chaidze@gmail.com>
2024-08-20 08:13:31 -0700

    commit

---
+++ day16/homework/00.py
@@ -0,0 +1,17 @@
+'''i=10
+while i <= 30:
+    print(i)
+    i=i+1
+a=50
+while a >=10:
+    print(a)
+    a=a-1
+b=0
+while b <10:
+    print("goa best")
+    b=b+1'''
+sum=0
+while sum !=55:
+    for i in range(11):
+        sum=sum + i
+        print(sum)
---
+++ day16/homework/01.py
@@ -0,0 +1,30 @@
+for i in range(10):
+    print(i)
+for i in range(20):
+    print(i)
+for i in range(10):
+    print("goa best")
+for i in range(20):
+    print(i/2)
+for i in range(15):
+    print(i*2)
+
+num=7
+guess=int(input("guess the number:"))
+while guess != num:
+    guess=int(input("guess the number:"))
+i=0
+while i<10:
+    print("mary")
+    i=i+1
+hi = 9
+while hi>0:
+    hi=hi - 1
+    print(hi)
+yes="yes"
+no="no"
+while ask != yes and ask!=no:
+    ask=input("do you want to continue?")
+while num != hi:
+    print("ok")
+    num = num +1

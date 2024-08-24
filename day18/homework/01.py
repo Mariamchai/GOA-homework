b9704374ad72dd39d0116701c6f3ae742e8dfc62
Mariamchai <mari.chaidze@gmail.com>
2024-08-24 07:37:52 -0700

    commit

---
+++ day18/homework/01.py
@@ -0,0 +1,15 @@
+add=1+1
+sub=1-1
+mult=1*1
+divide=1//1
+print(add, sub, mult, divide)
+one="threre's 6 more assaignments"
+two="which is focused on operations"
+three="without using them"
+print(one, two, three)
+h=1
+i=2
+print(h>i, h<i, h==i, h>=i, h<=i, h!=i)
+age=1
+age1=2
+print(age>age1, age<age1, age==age1)

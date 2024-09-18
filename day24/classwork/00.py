4a1993f34c7538792133b5a5827a702f38c2acbd
Mariamchai <mari.chaidze@gmail.com>
2024-09-18 22:30:14 +0400

    commit

---
+++ day24/classwork/00.py
@@ -0,0 +1,18 @@
+list=[1,2,3,4,5]
+print(list[2:4])
+ask=input (int("enter a number:"))
+if ask==1:
+    print(list[0])
+elif ask ==2:
+    print(list[1])
+elif ask ==3:
+    print(list[2])
+elif ask==4:
+    print(list[3])
+elif ask==5:
+    print(list[4])
+else:
+    print("try something else")
+    
+
+
---
+++ day24/homework/01.py
@@ -0,0 +1,19 @@
+number=[2,3,9]
+sum=0
+for i in number:
+    sum=sum+i
+print(sum)
+
+hi="hello world"
+for i in hi:
+    print(i)
+a=1
+while a <= 100:
+    print(a)
+    a=a+1
+
+
+
+
+
+

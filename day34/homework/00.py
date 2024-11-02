8ec95e24d0ff6528286ec65893472c55ff3ff4d0
Mariamchai <mari.chaidze@gmail.com>
2024-11-02 22:19:22 +0400

    commit

---
+++ day34/homework/00.py
@@ -0,0 +1,24 @@
+def boombayah(num):
+    num1=num+5
+    print(num1)
+boombayah(5)
+def multi(num1,num2):
+    print(num1*num2)
+multi(1,2)
+def hi(h):
+    print(len(h))
+hi("wowzers")
+def zoo():
+    wee=["h","i","o"]
+    print(len(wee))
+zoo()
+def palindrome(erm):
+    if erm == erm[::-1]:
+        print(True)
+    else:
+        print(False)
+palindrome("hannah")
+def idk():
+    ask = input("type any word")
+    print(ask.upper())
+idk()
---
+++ day38/homework/00.py
@@ -0,0 +1,22 @@
+a=[0,1,2,3,4]
+b=[1,2,3,4,5]
+c=[2,3,4,5,6]
+print(max(a),max(b),max(c))
+print(min(a),min(b),min(c))
+print(len(a),len(b),len(c))
+print(sum(a),sum(b),sum(c))
+
+
+d=[1,2,3,4,5,6,7,8,9,0]
+e=[2,3,4,5,6,7,8,9,0,1]
+f=[3,4,5,6,7,8,9,0,1,2]
+g=[4,5,6,7,8,9,0,1,2,3]
+print(d[3::])
+for i in e:
+    print(e[3:8])
+    break
+print(f[-2:-6])
+while g !=g[9]:
+    print(g[0])
+    g=g[0+1]
+

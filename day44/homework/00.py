d9249633433041c30becd0aab368e5917ebfa41f
Mariamchai <mari.chaidze@gmail.com>
2024-11-24 11:52:42 +0400

    commit

---
+++ day44/homework/00.py
@@ -0,0 +1,34 @@
+print("hello world")
+
+def sum(num,num1):
+    print(int(num)+int(num1))
+sum(1,2)
+
+print("hi"+"bye") #concatenation is when you connect one thing to another
+
+print(6/2) #this will come es a float because we used the "/" only one time instead of twice and this conversion is implicit
+
+1<2, 2>1, 2==2, 2!=3, 1<=2, 2>=1
+
+5 + 5 == 8  + 2
+
+print(4+6==10 and 5+5==10)
+print(4+5==9 and 9+9==16)
+print(1+1==2 or 2+2==4)
+print(4+5==9 or 9+9==16)
+print(4+5==10 and 9+9==16)
+
+for i in range(1,11):
+    print(i)
+
+a="hello world"
+for i in a:
+    print(i)
+
+i=1
+while i <= 10:
+    print(i)
+    i+=1
+    
+
+

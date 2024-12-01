10f1b5493928efdb687498aaac78faaa02db0dbc
Marichai <mari.chaidze@gmail.com>
2024-12-01 15:10:23 +0400

    main

---
+++ day16/classwork/00.py
@@ -0,0 +1,16 @@
+i= 0
+while i <= 10:
+    print(i)
+    i= i+1
+k=20
+while k >= 10:
+    print(k)
+    k=k-1
+x=0
+while x !=5:
+    print("goa")
+secret="hi"
+secret1=input("guess the word")
+while secret != secret1:
+    secret1=input("guess the word")
+print("matched")
---
+++ day16/homework/00.py
@@ -0,0 +1,12 @@
+i=10
+while i <= 30:
+    print(i)
+    i=i+1
+a=50
+while a >=10:
+    print(a)
+    a=a-1
+b=0
+while b <10:
+    print("goa best")
+    b=b+1

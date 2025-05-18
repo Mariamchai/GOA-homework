b289e5f5c3a382a9c98cf1e714d2261ae0de5300
Mariamchai <mari.chaidze@gmail.com>
2025-05-18 13:15:54 +0400

    commit

---
+++ day36/classwork/00.py
@@ -0,0 +1,4 @@
+def max_of_two(num1,num2):
+    n=[num1,num2]
+    print (max(n))
+max_of_two(3,5)
---
+++ day94/classwork/00.js
@@ -0,0 +1,15 @@
+function Person(name, lastname, age) {
+  this.name = name;
+  this.lastname = lastname;
+  this.age = age;
+}
+
+
+const person1 = new Person("gio", "gagoSidze", 13);
+const person2 = new Person("Nino", "GviniaSvili", 13);
+const person3 = new Person("mari", "gelaSvili", 13);
+
+
+console.log(person1);
+console.log(person2);
+console.log(person3);

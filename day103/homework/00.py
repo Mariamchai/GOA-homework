6668b1d55e0e83e80d5027617753926e2303172c
Mariamchai <mari.chaidze@gmail.com>
2025-06-26 02:14:42 -0700

    commit

---
+++ day103/homework/00.py
@@ -0,0 +1,4 @@
+user=input("enter a sentence")
+list = user.split()
+for i in list:
+    print(i)
---
+++ day105/classwork/00.js
@@ -0,0 +1,4 @@
+let name = ["hi", 12, True]
+console.log(name[0])
+console.log(name[1])
+console.log([name[2]])
